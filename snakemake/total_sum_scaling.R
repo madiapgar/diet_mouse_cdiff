7bc42bfa0a8cc0a348066b87aadff8ab5f902c11
madiapgar <madi.apgar@gmail.com>
2023-06-29 16:46:47 -0600

    fixing metabolomics plot/stats

---
+++ snakemake/total_sum_scaling.R
@@ -0,0 +1,51 @@
+## 6-29-23
+## total sum scaling of 16S rDNA data 
+## since we can't rarefy it due to lactococcus contamination
+
+## needed libraries 
+library(ape)
+library(ggpubr)
+library(magrittr)
+library(qiime2R)
+library(tidyverse)
+library(broom)
+library(rstatix)
+# library(AICcmodavg)
+
+# change to scripts directory if not there already
+curr_dir <- getwd()
+curr_dir <- str_split(curr_dir, '\\/')
+if (curr_dir[length(curr_dir)] != 'scripts'){
+  setwd('./scripts')
+}
+
+## input file paths
+biom_fp <- '../data/misc/euk_filt_mergedDietAim1table_051523-Copy1.qza'
+lacto_asv_fp <- '../data/misc/lactoOnlydna-sequences.fasta'
+
+## reading in raw biom table for normalization
+biom_pre <- read_qza(biom_fp)
+biom_pre <- biom_pre$data
+
+## reading in file with the lactococcus asvs
+lacto_asv <- read.FASTA(lacto_asv_fp)
+lacto_asv <- names(lacto_asv)
+
+## filtering lactococcus asvs out of the pre-total sum scaled biom table 
+biom_pre %>% 
+  as_tibble(rownames = 'asv') %>% 
+  filter(!(asv %in% lacto_asv)) -> biom 
+
+## total sum scaling (normalization) steps 
+biom %>% 
+  gather(-asv, key = sampleid, value = abund) %>% 
+  group_by(sampleid) %>% 
+  mutate(rel_abund = abund/sum(abund),
+         big_abund = rel_abund*100000,
+         big_abund = round(big_abund)) %>% 
+  select(asv, sampleid, big_abund) %>% 
+  spread(sampleid, big_abund) -> outtab
+
+## writing out total sum scaled table as a .tsv
+write_tsv(outtab,
+          '../data/qiime/total_sum_scaling.tsv')

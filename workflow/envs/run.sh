5c47b8735de85c9e664b2faa610add706ce8e4e9
madiapgar <madi.apgar@gmail.com>
2023-07-27 17:27:15 -0600

    snakemake environment updates

---
+++ workflow/envs/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o
+
+echo "--------creating qiime environment"
+conda update conda
+conda install wget 
+wget https://data.qiime2.org/distro/core/qiime2-2023.5-py38-osx-conda.yml
+CONDA_SUBDIR=osx-64 conda env create -n qiime2_testing --file qiime2-2023.5-py38-osx-conda.yml
+conda config --env --set subdir osx-64
+rm qiime2-2023.5-py38-osx-conda.yml
+
+echo "--------creating R envrionment"
+conda env create -f workflow/envs/r_env.yml
+
+echo "--------creating picrust environment"
+conda env create -f workflow/envs/picrust2.yml

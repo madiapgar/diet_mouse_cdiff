691a32fef84d20b84e880808b5a7d001863c2cf1
madiapgar <madi.apgar@gmail.com>
2023-07-28 13:46:05 -0600

    still messing with workflow environments

--- workflow/envs/run.sh
+++ workflow/envs/run.sh
@@ -6,11 +6,12 @@ set -o
 
 echo "--------creating qiime environment"
 conda update conda
-conda install wget 
+conda install wget
 wget https://data.qiime2.org/distro/core/qiime2-2023.5-py38-osx-conda.yml
 CONDA_SUBDIR=osx-64 conda env create -n qiime2_testing --file qiime2-2023.5-py38-osx-conda.yml
 conda config --env --set subdir osx-64
 rm qiime2-2023.5-py38-osx-conda.yml
+pip install snakemake
 
 echo "--------creating R envrionment"
 conda env create -f workflow/envs/r_env.yml

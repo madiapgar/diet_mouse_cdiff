25137384cd16dc264e8e1d5e78c43b49bc98c2b8
madiapgar <madi.apgar@gmail.com>
2023-07-31 14:04:51 -0600

    updating snakemake environments

--- workflow/envs/run.sh
+++ workflow/envs/run.sh
@@ -5,8 +5,10 @@ set -u
 set -o
 
 echo "--------creating qiime environment"
-conda update conda
-conda install wget
+## conda update conda
+## conda install wget
+## make sure that the qiime installation that you have in here is for the proper software system!! 
+## these instructions are for apple silicon (M1 and M2 chips)
 wget https://data.qiime2.org/distro/core/qiime2-2023.5-py38-osx-conda.yml
 CONDA_SUBDIR=osx-64 conda env create -n qiime2_testing --file qiime2-2023.5-py38-osx-conda.yml
 conda config --env --set subdir osx-64

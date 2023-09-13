932916fb1119a56cdd4a3e4c0ecb750a92ea315a
madiapgar <madi.apgar@gmail.com>
2023-09-13 10:35:29 -0600

    testing upload

--- workflow/envs/run.sh
+++ workflow/envs/run.sh
@@ -10,13 +10,13 @@ echo "--------creating qiime environment"
 ## make sure that the qiime installation that you have in here is for the proper software system!! 
 ## these instructions are for apple silicon (M1 and M2 chips)
 wget https://data.qiime2.org/distro/core/qiime2-2023.5-py38-osx-conda.yml
-CONDA_SUBDIR=osx-64 conda env create -n qiime2_testing --file qiime2-2023.5-py38-osx-conda.yml
+CONDA_SUBDIR=osx-64 conda env create -n qiime2-2023.5 --file qiime2-2023.5-py38-osx-conda.yml
 conda config --env --set subdir osx-64
 rm qiime2-2023.5-py38-osx-conda.yml
 pip install snakemake
 
 echo "--------creating R envrionment"
-conda env create -f workflow/envs/r_env.yml
+conda env create -f r_env.yml
 
 echo "--------creating picrust environment"
-conda env create -f workflow/envs/picrust2.yml
+conda env create -f picrust2.yml

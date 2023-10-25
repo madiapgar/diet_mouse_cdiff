f7c533c5d8ef9e22d3726809b75c41b77ae10981
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2023-10-24 18:56:57 -0600

    updating cecal analysis workflow

--- workflow/envs/run_alpine.sh
+++ workflow/envs/run_alpine.sh
@@ -10,7 +10,9 @@ conda install wget
 wget https://data.qiime2.org/distro/core/qiime2-2023.5-py38-linux-conda.yml
 conda env create -n qiime2-2023.5 --file qiime2-2023.5-py38-linux-conda.yml
 rm qiime2-2023.5-py38-linux-conda.yml
+conda activate qiime2-2023.5
 pip install snakemake
+conda deactivate
 
 echo "--------creating R envrionment"
 conda env create -f r_env.yml

67dd427fd0da83ce01154038b555653baa9ee6df
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2023-10-26 17:58:59 -0600

    more ancombc updates

---
+++ cecum/workflow/run_alpine.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o
+
+echo "--------creating qiime environment"
+## conda update conda
+conda install wget
+wget https://data.qiime2.org/distro/core/qiime2-2023.5-py38-linux-conda.yml
+conda env create -n qiime2-2023.5 --file qiime2-2023.5-py38-linux-conda.yml
+rm qiime2-2023.5-py38-linux-conda.yml
+conda activate qiime2-2023.5
+pip install snakemake
+conda deactivate
+
+echo "--------creating R envrionment"
+conda env create -f r_env.yml
+
+echo "--------creating picrust environment"
+conda env create -f picrust2.yml

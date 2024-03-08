ab7d4c73eb0881412c3238f20aae2c06f4ca98f2
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2024-03-07 17:04:43 -0700

    colon script clean up

--- colon/scripts/alpha_div_plots.R
+++ colon/scripts/alpha_div_plots.R
@@ -60,71 +60,45 @@ diet_names_labels <- c('Chow',
 
 ## needed functions (in order)
 ## 1
-## faith's pd plot 
-## assumes that the files is a .tsv
-faith_pd_plot <- function(faith_fp,
-                          metadata_file,
-                          labels,
-                          names_labels,
-                          title){
-  faith <- read_tsv(faith_fp)
-  names(faith)[names(faith) == '#SampleID'] <- 'sampleid'
+## alpha diversity plot function
+alpha_div_plot <- function(alpha_div_fp,
+                           rename_me,
+                           metadata_file,
+                           labels,
+                           names_labels,
+                           x_axis,
+                           y_axis,
+                           box_grouped_by,
+                           line_grouped_by,
+                           facet_by,
+                           title,
+                           x_label,
+                           y_label){
+  alpha_table <- read_tsv(alpha_div_fp)
+  names(alpha_table)[names(alpha_table) == rename_me] <- 'sampleid'
   metadata_file %>% 
-    left_join(faith, by = 'sampleid') -> faith_pd
+    left_join(alpha_table, by = 'sampleid') %>% 
+    filter(!is.na(diet)) -> joint_table
   ## what you want the grid labels to be (list)
   labs <- (labels) 
   ## what grid labels currently are (list)
   names(labs) <- (names_labels) 
-  faith_pd %>%
-    filter(!is.na(diet)) %>% 
-    ggplot(aes(x = day_post_inf, y = faith_pd)) +
-    geom_boxplot(aes(group = day_post_inf), outlier.shape = NA) +
-    geom_line(aes(group = mouse_id), alpha = 0.1) +
+  joint_table %>%
+    ggplot(aes(x = .data[[x_axis]], y = .data[[y_axis]])) +
+    geom_boxplot(aes(group = .data[[box_grouped_by]]), outlier.shape = NA) +
+    geom_line(aes(group = .data[[line_grouped_by]]), alpha = 0.1) +
     geom_vline(xintercept = -3, linetype = 'dashed', color = 'red', size = 0.2) +
     geom_vline(xintercept = 0, linetype = 'dashed', color = 'purple', size = 0.2) +
     geom_jitter(width = 0.1, height = 0, alpha = 0.4) +
     scale_x_continuous(breaks = c(-15, -8, -3, 0, 3)) +
     geom_smooth(se = FALSE) +
-    theme_bw(base_size = 16) +
-    facet_grid(~diet, labeller = labeller(diet = labs) ) +
+    theme_bw(base_size = 20) +
+    facet_grid(~.data[[facet_by]], 
+               labeller = labeller(.cols = labs) ) +
     ggtitle(title) +
-    xlab('Days Relative to Infection') +
-    ylab("Faith's PD") -> faith_plot
-  return(faith_plot)
-}
-
-## 2
-## shannon entropy plot
-## assumes that the file is a .tsv
-shannon_plot <- function(shannon_fp,
-                         metadata_file,
-                         labels,
-                         names_labels,
-                         title){
-  shannon <- read_tsv(shannon_fp)
-  names(shannon)[names(shannon) == '...1'] <- 'sampleid'
-  metadata_file %>% 
-    left_join(shannon, by = 'sampleid') -> shannon_entropy
-  ## what you want the grid labels to be (list)
-  labs <- (labels) 
-  ## what grid labels currently are (list)
-  names(labs) <- (names_labels) 
-  shannon_entropy %>%
-    filter(!is.na(diet)) %>% 
-    ggplot(aes(x = day_post_inf, y = shannon_entropy)) +
-    geom_boxplot(aes(group = day_post_inf), outlier.shape = NA) +
-    geom_line(aes(group = mouse_id), alpha = 0.1) +
-    geom_vline(xintercept = -3, linetype = 'dashed', color = 'red', size = 0.2) +
-    geom_vline(xintercept = 0, linetype = 'dashed', color = 'purple', size = 0.2) +
-    geom_jitter(width = 0.1, height = 0, alpha = 0.4) +
-    scale_x_continuous(breaks = c(-15, -8, -3, 0, 3)) +
-    geom_smooth(se = FALSE) +
-    theme_bw(base_size = 16) +
-    facet_grid(~diet, labeller = labeller(diet = labs) ) +
-    ggtitle(title) +
-    xlab('Days Relative to Infection') +
-    ylab("Shannon Entropy") -> shannon_plot
-  return(shannon_plot)
+    xlab(x_label) +
+    ylab(y_label) -> plot
+  return(plot)
 }
 
 ## core metrics file prep
@@ -133,21 +107,43 @@ metadata <- read_tsv(args$metadata_FP)
 
 ## faith's pd plot 
 faith_title <- "Faith's Phylogenetic Diversity"
+faith_renamed <- '#SampleID'
+faith_x_lab <- 'Days Relative to Infection'
+faith_y_lab <- "Faith's PD"
 
-faith_plot <- faith_pd_plot(args$faith_pd_FP,
-                            metadata,
-                            diet_labs,
-                            diet_names_labels,
-                            faith_title)
+faith_plot <- alpha_div_plot(alpha_div_fp = args$faith_pd_FP,
+                             rename_me = faith_renamed,
+                             metadata_file = metadata,
+                             labels = diet_labs,
+                             names_labels = diet_names_labels,
+                             x_axis = 'day_post_inf',
+                             y_axis = 'faith_pd',
+                             box_grouped_by = 'day_post_inf',
+                             line_grouped_by = 'mouse_id',
+                             facet_by = 'diet',
+                             title = faith_title,
+                             x_label = faith_x_lab,
+                             y_label = faith_y_lab)
 
 ## shannon entropy plot 
 shannon_title <- "Shannon Entropy"
+shannon_renamed <- '...1'
+shannon_x_lab <- 'Days Relative to Infection'
+shannon_y_lab <- 'Shannon Entropy'
 
-shannon_entropy_plot <- shannon_plot(args$shannon_FP,
-                                     metadata,
-                                     diet_labs,
-                                     diet_names_labels,
-                                     shannon_title)
+shannon_entropy_plot <- alpha_div_plot(alpha_div_fp = args$shannon_FP,
+                                       rename_me = shannon_renamed,
+                                       metadata_file = metadata,
+                                       labels = diet_labs,
+                                       names_labels = diet_names_labels,
+                                       x_axis = 'day_post_inf',
+                                       y_axis = 'shannon_entropy',
+                                       box_grouped_by = 'day_post_inf',
+                                       line_grouped_by = 'mouse_id',
+                                       facet_by = 'diet',
+                                       title = shannon_title,
+                                       x_label = shannon_x_lab,
+                                       y_label = shannon_y_lab)
 
 ## saving my plot outputs to the plots folder
 ggsave(args$output_faith_FP,

bb7a1a92619af198676c5fa541a97d5a4601f56c
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2024-05-30 14:33:50 -0600

    day -15 and 3 sample analysis

---
+++ new_experiments/scripts/alpha_div_plots.R
@@ -0,0 +1,162 @@
+## 6-26-23 
+## Qiime2 core metrics alpha diversity analysis output plot construction
+## Faith's PD plot
+## Shannon Entropy plot
+
+## needed libraries
+library(ggpubr)
+library(ggplot2)
+library(magrittr)
+library(qiime2R)
+library(tidyverse)
+library(broom)
+library(cowplot)
+library(viridis)
+library(argparse)
+
+## using argparse for my file paths
+## so I can easily edit file paths from my workflow and not have to edit the actual R scripts
+parser <- ArgumentParser()
+parser$add_argument("-m",
+                    "--metadata",
+                    dest = "metadata_FP",
+                    help = "Filepath to metadata file in .tsv format.")
+parser$add_argument("-f",
+                    "--faith_pd",
+                    dest = "faith_pd_FP",
+                    help = "Filepath to Faith's PD file in .tsv format.")
+parser$add_argument("-s",
+                    "--shannon",
+                    dest = "shannon_FP",
+                    help = "Filepath to Shannon Entropy file in .tsv format.")
+parser$add_argument("-of",
+                    "--output_faith",
+                    dest = "output_faith_FP",
+                    help = "Filepath to Faith's PD plot in .pdf format.")
+parser$add_argument("-os",
+                    "--output_shannon",
+                    dest = "output_shannon_FP",
+                    help = "Filepath to Shannon Entropy plot in .pdf format.")
+
+args <- parser$parse_args()
+
+## input file paths 
+# metadata_FP <- './data/misc/processed_metadata.tsv'
+# faith_pd_FP <- './data/qiime/core_outputs/faith_pd.tsv'
+# shannon_FP <- './data/qiime/core_outputs/shannon_entropy.tsv'
+
+diet_labs <- 
+  c('Chow', 
+    'High Fat / High Fiber', 
+    'High Fat / Low Fiber', 
+    'Low Fat / High Fiber', 
+    'Low Fat / Low Fiber')
+
+diet_names_labels <- c('Chow', 
+                       'HF/HF', 
+                       'HF/LF', 
+                       'LF/HF', 
+                       'LF/LF')
+
+## needed functions (in order)
+## 1
+## faith's pd plot 
+## assumes that the files is a .tsv
+faith_pd_plot <- function(faith_fp,
+                          metadata_file,
+                          labels,
+                          names_labels,
+                          title){
+  faith <- read_tsv(faith_fp)
+  names(faith)[names(faith) == '#SampleID'] <- 'sampleid'
+  metadata_file %>% 
+    left_join(faith, by = 'sampleid') -> faith_pd
+  ## what you want the grid labels to be (list)
+  labs <- (labels) 
+  ## what grid labels currently are (list)
+  names(labs) <- (names_labels) 
+  faith_pd %>%
+    filter(!is.na(diet)) %>% 
+    ggplot(aes(x = day_post_inf, y = faith_pd)) +
+    geom_boxplot(aes(group = day_post_inf), outlier.shape = NA) +
+    geom_line(aes(group = mouse_id), alpha = 0.1) +
+    geom_vline(xintercept = -3, linetype = 'dashed', color = 'red', size = 0.2) +
+    geom_vline(xintercept = 0, linetype = 'dashed', color = 'purple', size = 0.2) +
+    geom_jitter(width = 0.1, height = 0, alpha = 0.4) +
+    scale_x_continuous(breaks = c(-15, -8, -3, 0, 3)) +
+    geom_smooth(se = FALSE) +
+    theme_bw(base_size = 16) +
+    facet_grid(~diet, labeller = labeller(diet = labs) ) +
+    ggtitle(title) +
+    xlab('Days Relative to Infection') +
+    ylab("Faith's PD") -> faith_plot
+  return(faith_plot)
+}
+
+## 2
+## shannon entropy plot
+## assumes that the file is a .tsv
+shannon_plot <- function(shannon_fp,
+                         metadata_file,
+                         labels,
+                         names_labels,
+                         title){
+  shannon <- read_tsv(shannon_fp)
+  names(shannon)[names(shannon) == '...1'] <- 'sampleid'
+  metadata_file %>% 
+    left_join(shannon, by = 'sampleid') -> shannon_entropy
+  ## what you want the grid labels to be (list)
+  labs <- (labels) 
+  ## what grid labels currently are (list)
+  names(labs) <- (names_labels) 
+  shannon_entropy %>%
+    filter(!is.na(diet)) %>% 
+    ggplot(aes(x = day_post_inf, y = shannon_entropy)) +
+    geom_boxplot(aes(group = day_post_inf), outlier.shape = NA) +
+    geom_line(aes(group = mouse_id), alpha = 0.1) +
+    geom_vline(xintercept = -3, linetype = 'dashed', color = 'red', size = 0.2) +
+    geom_vline(xintercept = 0, linetype = 'dashed', color = 'purple', size = 0.2) +
+    geom_jitter(width = 0.1, height = 0, alpha = 0.4) +
+    scale_x_continuous(breaks = c(-15, -8, -3, 0, 3)) +
+    geom_smooth(se = FALSE) +
+    theme_bw(base_size = 16) +
+    facet_grid(~diet, labeller = labeller(diet = labs) ) +
+    ggtitle(title) +
+    xlab('Days Relative to Infection') +
+    ylab("Shannon Entropy") -> shannon_plot
+  return(shannon_plot)
+}
+
+## core metrics file prep
+## metadata file prep
+metadata <- read_tsv(args$metadata_FP)
+names(metadata)[names(metadata) == '#SampleID'] <- 'sampleid'
+
+## faith's pd plot 
+faith_title <- "Faith's Phylogenetic Diversity"
+
+faith_plot <- faith_pd_plot(args$faith_pd_FP,
+                            metadata,
+                            diet_labs,
+                            diet_names_labels,
+                            faith_title)
+
+## shannon entropy plot 
+shannon_title <- "Shannon Entropy"
+
+shannon_entropy_plot <- shannon_plot(args$shannon_FP,
+                                     metadata,
+                                     diet_labs,
+                                     diet_names_labels,
+                                     shannon_title)
+
+## saving my plot outputs to the plots folder
+ggsave(args$output_faith_FP,
+       plot = faith_plot, 
+       width = 6, 
+       height = 4.5)
+
+ggsave(args$output_shannon_FP,
+       plot = shannon_entropy_plot, 
+       width = 6, 
+       height = 4.5)
---
+++ new_experiments/scripts/family_abun_plots.R
@@ -0,0 +1,174 @@
+## 7-14-23
+## putting together my microbe family abundance plots based on the 16S rDNA data
+## a different way of visualizing the taxa barplot (might be easier)
+## you have two options
+
+## needed libraries
+library(ggpubr)
+library(ggplot2)
+library(magrittr)
+library(qiime2R)
+library(tidyverse)
+library(broom)
+library(cowplot)
+library(vegan)
+library(viridis)
+library(argparse)
+
+## using argparse for my file paths
+## so I can easily edit file paths from my workflow and not have to edit the actual R scripts
+parser <- ArgumentParser()
+parser$add_argument("-m",
+                    "--metadata",
+                    dest = "metadata_FP",
+                    help = "Filepath to metadata file in .tsv format.")
+parser$add_argument("-o",
+                    "--otu",
+                    dest = "otu_table_FP",
+                    help = "Filepath to OTU Table file in .qza format.")
+parser$add_argument("-t",
+                    "--taxonomy",
+                    dest = "tax_FP",
+                    help = "Filepath to taxonomy file in .qza format.")
+parser$add_argument("-p1",
+                    "--plot1",
+                    dest = "plot1_FP",
+                    help = "Filepath to first family abundance plot in .pdf format.")
+parser$add_argument("-p2",
+                    "--plot2",
+                    dest = "plot2_FP",
+                    help = "Filepath to second family abundance plot in .pdf format.")
+
+args <- parser$parse_args()
+
+## input file paths and others
+# otu_table_FP <- './data/qiime/taxonomy_filtered.qza'
+# tax_FP <- './data/qiime/taxonomy.qza'
+# metadata_FP <- './data/misc/processed_metadata.tsv'
+diet_labs <- 
+  c('Chow', 
+    'High Fat / High Fiber', 
+    'High Fat / Low Fiber', 
+    'Low Fat / High Fiber', 
+    'Low Fat / Low Fiber')
+names(diet_labs) <- c('Chow', 
+                      'HF/HF', 
+                      'HF/LF', 
+                      'LF/HF', 
+                      'LF/LF')
+wanted_level <- 'Family'
+wanted_family <- c('Enterobacteriaceae', 'Lactobacillaceae', 'Lachnospiraceae', 'Enterococcaceae',
+                   'Staphylococcaceae', 'Bacteroidaceae', 'Ruminococcaceae')
+
+## functions in order of usage 
+## 1
+family_abun_file_prep <- function(metadata_fp,
+                                  tax_fp,
+                                  otu_table_fp,
+                                  tax_level,
+                                  wanted_tax){
+  ## metadata
+  metadata <- read_tsv(metadata_fp)
+  names(metadata)[names(metadata) == '#SampleID'] <- 'sampleid'
+  ## taxonomy
+  taxonomy <- read_qza(tax_fp)$data %>% 
+    parse_taxonomy() %>% 
+    rownames_to_column('asv')
+  ## otu table 
+  otu_table <- read_qza(otu_table_fp)$data
+  otu_table %>% 
+    as_tibble(rownames = 'asv') %>% 
+    gather(-asv, key = sampleid, value = abun) %>% 
+    group_by(sampleid) %>% 
+    mutate(rel_abun = abun/sum(abun)) %>% 
+    mutate(rel_abun = rel_abun + 0.000001) -> otu_table
+  ## joining all tables together 
+  otu_table %>% 
+    left_join(metadata, by = 'sampleid') %>% 
+    left_join(taxonomy, by = 'asv') -> abun_table
+  abun_table %>% 
+    group_by(sampleid, day_post_inf, diet, mouse_id, 
+             purified_diet, high_fat, high_fiber, 
+             .data[[tax_level]]) %>% 
+    summarise(rel_abund = sum(rel_abun)) %>% 
+    filter(.data[[tax_level]] %in% wanted_tax) %>% 
+    mutate(mouse_fact = as.factor(mouse_id),
+           day_fact = as.factor(day_post_inf)) -> abun_filt
+  ## creating a list for my outputs
+  my_list <- list(Metadata = metadata,
+                  Taxonomy = taxonomy,
+                  OTUTable = otu_table,
+                  AbundanceTable = abun_filt)
+  return(my_list)
+}
+
+## 2 
+abun_plots <- function(abundance_table){
+  ## first plot
+  abundance_table %>%
+    filter(!is.na(diet)) %>% 
+    ggplot(aes(x = day_post_inf, y = rel_abund)) +
+    scale_y_continuous(trans = 'log10') +
+    scale_x_continuous(breaks = c(-15, 3)) +
+    geom_boxplot(aes(group = day_post_inf), outlier.shape = NA) +
+    geom_vline(xintercept = -3, linetype = 'dashed', color = 'red', size = 0.2) +
+    geom_vline(xintercept = 0, linetype = 'dashed', color = 'purple', size = 0.2) +
+    geom_line(aes(group = mouse_id), alpha = 0.1) +
+    geom_smooth(se = FALSE) +
+    geom_jitter(width = 0.1, height = 0, alpha = 0.4) +
+    theme_bw(base_size = 16) +
+    facet_grid(Family~diet, labeller = labeller(diet = diet_labs)) +
+    theme(strip.text.y = element_text(angle = 0)) +
+    ggtitle("Microbe Family Relative Abundance") +
+    ylab("Relative Abundance") +
+    xlab("Days Relative to Infection") -> family_abun1
+  ## second plot
+  abun_filt %>%
+    filter(!is.na(diet)) %>% 
+    ggplot(aes(x = mouse_fact, y = day_fact)) +
+    geom_tile(aes(fill = rel_abund), alpha = 0.5) +
+    scale_fill_viridis(option = "H", name = 'Relative\nAbundance') +
+    theme_bw(base_size = 16) +
+    facet_grid(Family~diet, scales = 'free',
+               labeller = labeller(diet = diet_labs)) +
+    theme(strip.text.y = element_text(angle = 0),
+          axis.text.x = element_blank()) + 
+    xlab("Mouse ID") +
+    ylab("Days Relative to Infection") +
+    scale_y_discrete(limits = rev) -> family_abun2
+  ## creating a list of my two plots
+  my_list <- list(FamilyAbundance1 = family_abun1,
+                  FamilyAbundance2 = family_abun2)
+  return(my_list)
+}
+
+## family abundance table prep 
+abun_files <- family_abun_file_prep(args$metadata_FP,
+                                    args$tax_FP,
+                                    args$otu_table_FP,
+                                    wanted_level,
+                                    wanted_family)
+
+## pulling the abundance table out, you can also take metadata, otu table, and taxonomic info
+## out of the list output 
+abun_filt <- abun_files$AbundanceTable
+
+## generating the plots
+family_abun_plots <- abun_plots(abun_filt)
+
+## scatter plot/ line graph by mouse id and wanted microbe families
+abun1 <- family_abun_plots$FamilyAbundance1
+## heat map by days relative to infection and mouse id for wanted microbe families 
+abun2 <- family_abun_plots$FamilyAbundance2
+
+## saving my plot outputs
+## option #1
+ggsave(args$plot1_FP,
+       plot = abun1, 
+       width = 12, 
+       height = 8)
+## option #2
+ggsave(args$plot2_FP,
+       plot = abun2, 
+       width = 12, 
+       height = 8)

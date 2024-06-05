ddb6f916f4b7be2891cb4a04566acd391a658d28
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2024-06-05 14:16:57 -0600

    config file updates

--- workflow/run_snakemake.sh
+++ workflow/run_snakemake.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## my command to run my snakemake workflow bc I'm lazy
-snakemake -s workflow/snakefile -c 7 --use-conda --keep-going --configfile workflow/newExp_comp_d15_config.yml
+snakemake -s workflow/snakefile -c 7 --use-conda --keep-going --configfile workflow/newExp_config.yml

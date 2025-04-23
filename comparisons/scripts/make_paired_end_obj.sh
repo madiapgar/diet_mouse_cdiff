52095732b3a0ae1d51f3881f62745b7aa7c1bd7d
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2025-04-23 10:55:06 -0600

    pulling comparisons off

--- comparisons/scripts/make_paired_end_obj.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "making an EMP Paired End Sequences object out of raw fastq files for QIIME2 analysis!"
-qiime tools import \
-    --type EMPPairedEndSequences \
-    --input-path comparisons/data/first_set_qiime/SEQ016/raw_seqs \
-    --output-path comparisons/data/first_set_qiime/SEQ016/oldExp_s3_paired_end_seqs.qza
